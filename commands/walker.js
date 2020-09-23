8dff4e60363c92ecfcebc0c1bbfc5b24b34a7c60
luketwd <71384528+luketwd@users.noreply.github.com>
2020-09-23 11:14:25 +0300

    Update walker.js

--- commands/walker.js
+++ commands/walker.js
@@ -10,7 +10,7 @@ module.exports = {
             return Math.floor(Math.random() * (max - min)) + min;
         }
         var msg = getRandomInt(0, 20);//20 cazuri de la 0 la 19
-        var gif = getRandomInt(0, 5);
+        var gif = getRandomInt(0, 6);
         var mesajembed;
         if(msg <= 3){//4 din 20 sansa
             mesajembed = "Here's some biters."
@@ -36,6 +36,9 @@ module.exports = {
         }
         if(gif == 4){
             gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754270659693117460/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.gif";
+        }
+        if(gif == 5){
+            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/758238531209658378/tenor_1.gif";
         }
 
         const walkerEmbed = new Discord.MessageEmbed()
