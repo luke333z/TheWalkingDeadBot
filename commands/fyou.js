75239cc98739f288c9afd4320e98076275ddde4d
luketwd <71384528+luketwd@users.noreply.github.com>
2020-09-23 11:33:36 +0300

    Update fyou.js

--- commands/fyou.js
+++ commands/fyou.js
@@ -10,16 +10,14 @@ module.exports = {
             return Math.floor(Math.random() * (max - min)) + min;
         }
         var url1;
-        var random = getRandomInt(0,5);
+        var random = getRandomInt(0,4);
         if(random == 0){
             url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237314371158046/tara_gif.gif";
         }else if(random == 1){
-            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758236730015875092/giphy_19.gif";
-        }else if(random == 2){
             url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758236871888601098/tenor_2.gif"
-        }else if(random == 3){
+        }else if(random == 2){
             url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237032337768468/tenor_3.gif"
-        }else if(random == 4){
+        }else if(random == 3){
             url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237088918405130/tenor_4.gif"
         }
         
