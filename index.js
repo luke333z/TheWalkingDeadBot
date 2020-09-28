c1486fe60c2e7b21d5cd16c84c16ddd30aff8aa8
luketwd <71384528+luketwd@users.noreply.github.com>
2020-09-28 21:03:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -94,8 +94,6 @@ client.on('message', message =>{
     
 });
 
-client.login("NzQ4MTA1NjY1MzAxNzA4ODgz.X0YlsQ.NAMour6hWLcjz5JVWbyjgWm-dl0" );
+client.login(process.env.token);
 
 
-//process.env.token
-//"NzQ4MTA1NjY1MzAxNzA4ODgz.X0YlsQ.NAMour6hWLcjz5JVWbyjgWm-dl0" 
