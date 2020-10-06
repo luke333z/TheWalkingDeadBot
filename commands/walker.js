3c7f0b13dd005a27d92608ab8fe0e6884b5be6c1
luketwd <71384528+luketwd@users.noreply.github.com>
2020-10-06 22:42:19 +0300

    Update walker.js

--- commands/walker.js
+++ commands/walker.js
@@ -26,19 +26,22 @@ module.exports = {
             gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754269504716013628/giphy_15.gif";
         }
         if(gif == 1){
-            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754269535032311808/giphy_16.gif";
+            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754269535032311808/giphy_16.gif";//morgans wife
         }
         if(gif == 2){
-            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754269577684320296/giphy_17.gif";
+            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754269577684320296/giphy_17.gif";//walker cut by carol while going to the kingdom?
         }
         if(gif == 3){
-            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754270424312971285/RemorsefulBadDuckbillplatypus-size_restricted.gif";
+            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754270424312971285/RemorsefulBadDuckbillplatypus-size_restricted.gif";//the bike guy
         }
         if(gif == 4){
-            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754270659693117460/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.gif";
+            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/754270659693117460/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.gif";//winslow kred
         }
         if(gif == 5){
-            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/758238531209658378/tenor_1.gif";
+            gifembed = "https://cdn.discordapp.com/attachments/705049194682908782/758238531209658378/tenor_1.gif";//walker girl, at the start
+        }
+        if(gif == 6){
+            gifembed = "https://cdn.discordapp.com/attachments/753567636968833103/763121371331952650/walkr.gif";//walkers start ricc cu kalu
         }
 
         const walkerEmbed = new Discord.MessageEmbed()
