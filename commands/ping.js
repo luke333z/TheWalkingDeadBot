4cf7c62bfe2c7041ae3cc91fe19eeff4be9a7080
luketwd <71384528+luketwd@users.noreply.github.com>
2020-10-07 20:55:49 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports = {
-    name: 'ping',
-    description: "Shows your ping.",
+    name: 'ping',//a name
+    description: "Shows your ping.",//a descrioption
     execute(message, args){
+//your code for the command
+
+
         var ping = Date.now() -  message.createdTimestamp + " ms";
         const pingEmbed = new Discord.MessageEmbed()
         .setColor("RANDOM")
