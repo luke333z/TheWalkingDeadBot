81a7327a720c6fdcb32526a18e95cfa0ac517d15
luketwd <71384528+luketwd@users.noreply.github.com>
2020-10-13 10:29:50 +0300

    Update insult.js

--- commands/insult.js
+++ commands/insult.js
@@ -10,17 +10,19 @@ module.exports = {
             return Math.floor(Math.random() * (max - min)) + min;
         }
         var url1;
-        var random = getRandomInt(0,5);
+        var random = getRandomInt(0,6);
         if(random == 0){
-            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237314371158046/tara_gif.gif";
+            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237314371158046/tara_gif.gif";//mishoun shi tara
         }else if(random == 1){
-            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758236871888601098/tenor_2.gif"
+            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758236871888601098/tenor_2.gif"//darul shi beth
         }else if(random == 2){
-            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237032337768468/tenor_3.gif"
+            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237032337768468/tenor_3.gif"//negan langa autobuz
         }else if(random == 3){
-            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237088918405130/tenor_4.gif"
+            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758237088918405130/tenor_4.gif"//negan in alt autobuz
         }else if(random == 4){
-            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758247813032181780/tenor_6.gif"
+            url1 = "https://cdn.discordapp.com/attachments/705049194682908782/758247813032181780/tenor_6.gif"//negan in aubtobuz
+        }else if(random == 5){
+            url1 = "https://cdn.discordapp.com/attachments/753567636968833103/765475256335990784/midlfingrif.gif"//jared cred
         }
         
         const purrson = message.mentions.users.first();
