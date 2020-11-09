013494f7fe342de3e6be09579098fc7f0e864a3d
luketwd <71384528+luketwd@users.noreply.github.com>
2020-11-09 20:35:02 +0200

    Update f.js

--- commands/f.js
+++ commands/f.js
@@ -4,11 +4,21 @@ module.exports = {
     name: 'f',
     description: "Pay your respects.",
     execute(message, args){
+        const negan = args.slice(0).join(" ");
+        if(!negan){
         const fEmbed = new Discord.MessageEmbed()
         .setColor("RANDOM")
         .setDescription(`${message.author.username}` + ` has paid their respects.`)
         .setTimestamp()
         .setFooter(process.env.DEVELOPER)
         message.channel.send(fEmbed);
+        } else {
+            const neganEmbed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`${message.author.username}` + ` has paid their respects for **${negan}**.`)
+            .setTimestamp()
+            .setFooter(process.env.DEVELOPER)
+            message.channel.send(neganEmbed);
+        }
     }
 }
