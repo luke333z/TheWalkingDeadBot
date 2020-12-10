03cca573b457cfaa6c11aef8f939f56cb3e44c14
luketwd <71384528+luketwd@users.noreply.github.com>
2020-12-10 21:00:49 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -13,7 +13,7 @@ module.exports = {
         .addField(`**Total Roles:**`, message.guild.roles.cache.size, true)
         .addField(`**Created on:**`, message.guild.createdAt, true)
         .addField('**Members:**', `Total: ${message.guild.memberCount}\nHumans: ${message.guild.members.cache.filter(m => !m.user.bot).size}\nBots: ${message.guild.members.cache.filter(m => m.user.bot).size}`, true)
-        .setThumbnail(message.guild.iconURL())
+        .setThumbnail(message.guild.iconURL({size: 2048, dynamic: true}))
         .setTimestamp()
         .setFooter(process.env.DEVELOPER);
         message.channel.send(serverinfoEmbed);
