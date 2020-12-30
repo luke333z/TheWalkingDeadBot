e2bae4ac7eb9406c7002684b06a5bc398d02de93
luketwd <71384528+luketwd@users.noreply.github.com>
2020-12-30 22:50:26 +0200

    Update loot.js

--- commands/loot.js
+++ commands/loot.js
@@ -18,7 +18,7 @@ module.exports = {
          
           
             
-            data = data.split('\r\n')
+            data = data.split('\n')
             var random = getRandomInt(0,data.length-1)
             
             
