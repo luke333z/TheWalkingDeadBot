512f5d4507f6149f84735d5618b8495532dafe55
luketwd <71384528+luketwd@users.noreply.github.com>
2021-01-04 12:24:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ client.on('message', async message =>{
           const embd = new Discord.MessageEmbed()
           .setTitle(`${user}'s OC has been approved!`)
           .setDescription(hi)
-          .setFooter("[Twd Server Manager](https://discord.gg/pqckK8q) • luke.#0003")
+          .addField("\n", "[Twd Server Manager](https://discord.gg/pqckK8q) • luke.#0003")
           .setTimestamp();
          
           
