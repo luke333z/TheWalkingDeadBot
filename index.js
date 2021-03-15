ef3313ee77817373ca33dcd91d06111f7b957134
luketwd <71384528+luketwd@users.noreply.github.com>
2021-03-15 09:46:51 +0200

    h

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ if(command === 'botinfo'){
     if(command === 'radd'){
         if(message.author.id != "332867444505051137") return;
         else{
-            const user = message.guild.member(message.author);
+            const user = message.guild.member(message.mentions.users.first());
             const gibRole = message.guild.roles.cache.find(r => r.name === `${args[0]}`)
             user.roles.add(gibRole)
             message.channel.send("Role Added")
